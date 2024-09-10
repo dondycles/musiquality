12c0a3fbe3b1dd3344918b614d2b4add5a83207d
dondycles <johnrod.dondoyano@gmail.com>
2024-09-10 17:10:39 +0800

    modified:   src/app/(non-arranger)/page.tsx

--- src/app/(non-arranger)/page.tsx
+++ src/app/(non-arranger)/page.tsx
@@ -1,13 +1,15 @@
-import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import TopSellingSheets from "@/components/top-selling-sheet";
 import TopArrangers from "@/components/top-arrangers";
 import SearchBar from "@/components/search-bar";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 px-4 lg:px-40 xl:px-64">
-      <SearchBar />
+      <Suspense>
+        <SearchBar />
+      </Suspense>
       <TopSellingSheets />
       <Separator />
       <TopArrangers />
